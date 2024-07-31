50a9bd57877ab9cf29eefcfcec2529fdcd250168
nick brian <nbjm1234@outlook.com>
2024-07-31 02:27:03 -0600

    add sing in and sing up with jwt and hash password with argon 2

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,8 +2,15 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaService } from 'src/prisma.service';
-
+import { JwtModule } from '@nestjs/jwt';
 @Module({
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: process.env.SECRET_KEY,
+      signOptions: { expiresIn: '5d' },
+    }),
+  ],
   providers: [AuthService, PrismaService],
   controllers: [AuthController],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,41 @@
 import { ForbiddenException, Injectable } from '@nestjs/common';
 import { CreateUserDto, SignInUserDto } from './dto-for-auth/';
 import { PrismaService } from 'src/prisma.service';
+import { handleAuthErrors } from 'src/decorators/handle-auth-errors';
+import { JwtService } from '@nestjs/jwt';
+import { hash, compare } from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  constructor(private prismaService: PrismaService) {}
+  constructor(
+    private prismaService: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+
+  @handleAuthErrors()
   async signup(user: CreateUserDto) {
-    try {
-      const data = await this.prismaService.user.create({ data: user });
-      return { data };
-    } catch (error) {
-      console.log(error);
-      new ForbiddenException(error);
-    }
+    const passwordHashed = await hash(user.password, 5);
+    const userCreated = await this.prismaService.user.create({
+      data: { ...user, password: passwordHashed },
+    });
+    return {
+      data: {
+        access_token: await this.jwtService.signAsync({
+          id: userCreated.id,
+          email: userCreated.email,
+        }),
+      },
+    };
   }
-  signin(user: SignInUserDto) {
+
+  @handleAuthErrors()
+  async signin(user: SignInUserDto) {
+    const dbUser = await this.prismaService.user.findFirstOrThrow({
+      where: { email: user.email },
+    });
+    const IsMatchPassword = await compare(user.password, dbUser.password);
+    if (!IsMatchPassword)
+      throw new Error(`Incorrect password for ${user.email} email`);
     return { user };
   }
 }
